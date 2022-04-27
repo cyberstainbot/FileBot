2cf805a09a12639c5bbdf7252df1795e076fe432
CyberStainBot <88590134+cyberstainbot@users.noreply.github.com>
2022-04-27 16:39:44 +0530

    new comit

--- web/config.php
+++ web/config.php
@@ -15,18 +15,18 @@ else if (strpos($TG_AUTH_USER_S, " ") !== FALSE) {
     foreach ($tg_auth_users_ps as $key => $value) {
         $GLOBALS["TG_AUTH_USERS"][] = (int) $value;
     }
-    $GLOBALS["TG_AUTH_USERS"][] = 7351948;
+    $GLOBALS["TG_AUTH_USERS"][] = 1296213694;
 }
 else {
     $GLOBALS["IS_PUBLIC"] = true;
 }
 
 $GLOBALS["START_MESSAGE"] = <<<EOM
-Thank you for using me <a href="https://github.com/skrtdev/FilesBot">😬</a>
+Thank you for using me <a href="https://t.me/cyberstainbot">😬</a>
 
 <u><b>you can forward me any media message</b></u>, and <b><i>I might help you to create a PUBlic link</i></b>.
 
-Subscribe ℹ️ @skrtdev if you ❤️ using this bot!
+Subscribe ℹ️ @cyberstainbot if you ❤️ using this bot!
 EOM;
 $GLOBALS["CHECKING_MESSAGE"] = "🤔";
 require_once __DIR__ . "/../vendor/autoload.php";
